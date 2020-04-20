6e2ac8a20cad41bcb432784ae74804a5366b51ac
Anand Rane <arane0017@gmail.com>
2020-04-20 19:10:26 +1000

    add fike

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,3 @@
-
-/**
- * Initialize the application.
- * Automatically called by the google maps API once it's loaded.
-*/
-function init() {
-  const initialPosition = { lat: 59.32, lng: 17.84 };
-  const map = createMap(initialPosition);
-  const marker = createMarker({ map, position: initialPosition });
-  const $info = document.getElementById('info');
-
-  let watchId = trackLocation({
-    onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
-      marker.setPosition({ lat, lng });
-      map.panTo({ lat, lng });
-      $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
-      $info.classList.remove('error');
-      $.ajax({
-        headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        type: "POST",
-        url: "/getlocation", 
-        data: {lat : lat, long: lng},
-        success: function(result){
-        console.log(result);
-      }});
-    },
-    onError: err => {
-      console.log($info);
-      $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
-      $info.classList.add('error');
-    }
-  });
-}
 /**
  * Create google maps Map instance.
  * @param {number} lat
@@ -45,7 +10,6 @@ const createMap = ({ lat, lng }) => {
     zoom: 15
   });
 };
-
 
 /**
  * Create google maps Marker instance.
@@ -68,12 +32,9 @@ const trackLocation = ({ onSuccess, onError = () => { } }) => {
     return onError(new Error('Geolocation is not supported by your browser.'));
   }
 
-  return navigator.geolocation.watchPosition(onSuccess, onError, {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  });
-};
+  setInterval(function(){
+  return navigator.geolocation.getCurrentPosition(onSuccess, onError)},180000);
+}
 
 /**
  * Get position error message from the given error code.
@@ -86,8 +47,44 @@ const getPositionErrorMessage = code => {
       return 'Permission denied.';
     case 2:
       return 'Position unavailable.';
-    case 3:
       return 'Timeout reached.';
+      case 3:
   }
 }
 
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function init() {
+  const initialPosition = { lat: 59.32, lng: 17.84 };
+  const map = createMap(initialPosition);
+  const marker = createMarker({ map, position: initialPosition });
+  const $info = document.getElementById('info');
+
+  
+    let watchId = trackLocation({
+      onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+        marker.setPosition({ lat, lng });
+        map.panTo({ lat, lng });
+        $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
+        $info.classList.remove('error');
+        $.ajax({
+          headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+          },
+          type: "POST",
+          url: "/getlocation", 
+          data: {lat : lat, long: lng},
+          success: function(result){
+          console.log(result);
+        }});
+      },
+      onError: err => {
+        console.log($info);
+        $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
+        $info.classList.add('error');
+      }
+    });
+  
+}
