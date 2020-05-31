da13f3ab7c39deea986697c4ea52c741b5c6ae18
arane0017 <arane0017@gmail.com>
2020-05-31 14:47:10 +1000

    some modification in convo

--- public/assets/js/prompt.js
+++ public/assets/js/prompt.js
@@ -32,42 +32,42 @@ window.onload = function(){
   setBusiness();
 }
 //
-// function getJson(category, array){
-//   var request = new XMLHttpRequest();
-//     let headers = new Headers();
-//
-//     headers.append('Content-Type', 'application/json');
-//     headers.append('Accept', 'application/json');
-//
-//     headers.append('Access-Control-Allow-Origin', '*');
-//     headers.append('Access-Control-Allow-Credentials', 'true');
-//   var requestURL = "https://newsapi.org/v2/top-headlines?country=au&category="+category+"&apiKey=8f6d87bb2fed4866a2e78c684776f53b";
-//   // const proxyurl = "https://cors-anywhere.herokuapp.com/";
-//   const url = requestURL;
-//   var result = fetch(url,  {  mode: 'no-cors', headers:headers});
-//   result.then(function(response) {
-//     return response.json();
-//   }).then(function(data) {
-//     getNews(array, data);
-//     getPrompts();
-//   });
-// }
 function getJson(category, array){
-    var request = new XMLHttpRequest();
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    var requestURL = "https://newsapi.org/v2/top-headlines?country=au&category="+category+"&apiKey=8f6d87bb2fed4866a2e78c684776f53b";
-    request.withCredentials = true;
-    request.open('GET',proxyurl + requestURL);
-    request.setRequestHeader('Access-Control-Allow-Headers','*');
-    request.setRequestHeader('Access-Control-Allow-Origin','*');
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        var jsonObj = request.response;
-        getNews(array, jsonObj);
-        getPrompts();
-    }
+  var request = new XMLHttpRequest();
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Credentials', 'true');
+  var requestURL = "https://newsapi.org/v2/top-headlines?country=au&category="+category+"&apiKey=8f6d87bb2fed4866a2e78c684776f53b";
+  // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = requestURL;
+  var result = fetch(url,  {  mode: 'no-cors', headers:headers});
+  result.then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    getNews(array, data);
+    getPrompts();
+  });
 }
+// function getJson(category, array){
+//     var request = new XMLHttpRequest();
+//     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//     var requestURL = "https://newsapi.org/v2/top-headlines?country=au&category="+category+"&apiKey=8f6d87bb2fed4866a2e78c684776f53b";
+//     request.withCredentials = true;
+//     request.open('GET',proxyurl + requestURL);
+//     request.setRequestHeader('Access-Control-Allow-Headers','*');
+//     request.setRequestHeader('Access-Control-Allow-Origin','*');
+//     request.responseType = 'json';
+//     request.send();
+//     request.onload = function() {
+//         var jsonObj = request.response;
+//         getNews(array, jsonObj);
+//         getPrompts();
+//     }
+// }
 
 function getNews(array, jsonObj) {
   var titles = jsonObj['articles'];
