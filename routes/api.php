2792f38e1ce62ec722bfcd234bed46ac749c5913
arane0017 <arane0017@gmail.com>
2020-05-31 17:42:16 +1000

    some modification in convo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, Control-Type, Authorization, X-Auth-Token');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+
 use Illuminate\Http\Request;
 
 /*
@@ -12,6 +16,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
