6c2e8e27a7f4fccb6848335fab81f6ddcdbaf85b
arane0017 <arane0017@gmail.com>
2020-05-31 13:41:05 +1000

    some modification in convo

--- public/assets/js/prompt.js
+++ public/assets/js/prompt.js
@@ -37,7 +37,7 @@ function getJson(category, array){
   var requestURL = "https://newsapi.org/v2/top-headlines?country=au&category="+category+"&apiKey=8f6d87bb2fed4866a2e78c684776f53b";
   // const proxyurl = "https://cors-anywhere.herokuapp.com/";
   const url = requestURL;
-  var result = fetch(url);
+  var result = fetch(url,  {  mode: 'no-cors'});
   result.then(function(response) {
     return response.json();
   }).then(function(data) {
